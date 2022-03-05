0ab0e85081bc8f465cb42b7367e229ecdfa2b97f
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2022-03-05 13:08:29 +0100

    Base Code
    The base code is here

---
+++ tankLogic.js
@@ -0,0 +1,14 @@
+importScripts('lib/tank.js');
+
+// Don't know where to start?
+// Read Getting Started in "Docs" section 
+
+tank.init(function(settings, info) {
+	// initialize tank here
+  
+});
+
+tank.loop(function(state, control) {
+	// write your tank logic here
+  
+});
