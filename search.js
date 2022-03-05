f3092e73c013d9b6c53185fe560e843120d78d54
alexandraPatica <alexandrapatica@yahoo.com>
2022-03-05 17:40:51 +0200

    Update in the search algorithm to shoot in "crazy" mode until the enemy is found, have a higher probability to hit the enemy. Put everything in a separate function.

--- search.js
+++ search.js
@@ -8,22 +8,25 @@ tank.init(function(settings, info) {
   turnTime = 0;
 });
 
-
-
-tank.loop(function(state, control) {
-	// write your tank logic here
-  //searchEnemy(state, control)
+function searchEnemy(state, control){
   if(!state.radar.enemy) {
+    	control.GUN_TURN = 1;
+    	control.SHOOT = 0.1;
       control.RADAR_TURN = 1;
     	if(state.collisions.wall || turnTime > 0 || state.radar.enemy) {
     control.THROTTLE = 0;
   } else {
     control.THROTTLE = 1;
+    
+    
+    //control.GUN_TURN = Math.deg.normalize(90 - state.angle); 
+    
   }
 
   if(state.collisions.wall) {
     // start turning when hitting a wall
     turnTime = 10;
+    
   }
 
   // keep turning whenever turn timer is above zero
@@ -37,10 +40,17 @@ tank.loop(function(state, control) {
   } else {
     control.RADAR_TURN = 0;
     
+    //let targetAngle = Math.deg.atan2(state.radar.enemy.y - state.y, state.radar.enemy.x - state.x);
+    
+    
     // Step #1
   let targetAngle = Math.deg.atan2(state.radar.enemy.y - state.y, state.radar.enemy.x - state.x);
   let bodyAngleDiff = Math.deg.normalize(targetAngle - state.angle);
   control.TURN = 0.5 * bodyAngleDiff;
+    
+    let gunAngle = Math.deg.normalize(targetAngle - state.angle);
+    let gunAngleDiff = Math.deg.normalize(gunAngle - state.gun.angle);
+    control.GUN_TURN = 0.3 * gunAngleDiff;
     
     let targetDistance = Math.distance(state.x, state.y, state.radar.enemy.x, state.radar.enemy.y);
   let distanceDiff = targetDistance - 150;
@@ -48,6 +58,12 @@ tank.loop(function(state, control) {
     
     control.SHOOT = 0.5;
   }
+}
+
+tank.loop(function(state, control) {
+	// write your tank logic here
+  searchEnemy(state, control)
+  
 });
 
 
